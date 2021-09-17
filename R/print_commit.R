f8cc9aa6fa7ea747cde654baecf8852a2635a93d
Eve Perry <eve@eveperry.com>
2021-09-16 17:44:09 -0700

    add class for commit label

--- R/print_commit.R
+++ R/print_commit.R
@@ -6,6 +6,6 @@
 #' @export
 print_commit = function() {
   if (git2r::in_repository()) {
-    paste("Commit:", substr(git2r::last_commit()[["sha"]], 1, 7))
+    paste("<span class=commit-label>Commit:", substr(git2r::last_commit()[["sha"]], 1, 7), "</span>")
   }
 }
