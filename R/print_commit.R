310e0981404d9d991697207d7dc9aeaaa39d29bc
Eve Perry <eve@eveperry.com>
2021-09-16 17:39:55 -0700

    Initial commit

---
+++ R/print_commit.R
@@ -0,0 +1,11 @@
+#' Print commit
+#'
+#' Prints the latest commit SHA
+#'
+#' @return "Commit: <last commit SHA>"
+#' @export
+print_commit = function() {
+  if (git2r::in_repository()) {
+    paste("Commit:", substr(git2r::last_commit()[["sha"]], 1, 7))
+  }
+}
